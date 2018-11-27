9008792594b3dc130b219c3deee1e4d0988d1710
Duncan <duncan_talbot@hotmail.com>
2018-11-27 16:23:23 +1100

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var items = document.getElementById("nav-links");
+  if (items.style.display === "block") {
+    items.style.display = "none";
+  } else {
+    items.style.display = "block";
+  }
+}
